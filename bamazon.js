d3b12db073415b41c910b864dad0476afb9194f1
Gage LaVergne <gagelavergne@Gages-MBP.lan>
2017-10-27 18:18:48 -0500

    udates to the prompts

--- bamazon.js
+++ bamazon.js
@@ -21,5 +21,45 @@ var makeTable = function(){
 		for(var i=0; i<res.length; i++){
 			console.log(res[i].itemid+" || "+res[i].productname+" || "+res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquantity+"\n");
 		}
+		promptCustomer(res);
+	})
+}
+
+var promptCustomer = function(res){
+	inquirer.prompt([{
+			type: 'input',
+			name: 'choice',
+			message: "What would you like to purchase?[Quit with Q]"
+	}]).then(function(answer){
+		var correct = false;
+		for(var i=0;i<res.length;i++){
+			if(res[i].productname==answer.choice){
+				correct = true;
+				var product=answer.choice;
+				var id=i;
+				inquirer.prompt({
+					type: 'input',
+					name: 'quant',
+					message: "How many would you like to buy?",
+					validate: function(value){
+						if(isNaN(value)==false){
+							return true;
+						} else {
+							return false;
+						}
+					}
+				}).then(function(answer){
+					if((res[id].stockquantity-answer.quant)>0){
+						conncetion.query("UPDATE products SET stockquantity='"+(res[id].stockquantity-answer.quant)+"' WHERE productname='"+product+"'", function(err,res2){
+							console.log("Product Bought!");
+							makeTable();
+						})
+					} else {
+						console.log("Not a valid selection.");
+						promptCustomer(res);
+					}
+				})
+			}
+		}
 	})
 }
